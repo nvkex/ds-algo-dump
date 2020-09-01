771c126f42403fd923a180f07e84835141997816
nvkex <scrbwrm@gmail.com>
2020-09-01 13:59:00 +0530

    Count Pairs with a difference of k

---
+++ Hackerrank Solutions/virtusa/Pairs.java
@@ -0,0 +1,51 @@
+import java.io.*;
+import java.util.*;
+
+public class Pairs {
+
+    // Complete the pairs function below.
+    static int pairs(int k, int[] arr) {
+        int c=0;
+        Hashtable<Integer,Integer> table = new Hashtable<Integer,Integer>();
+        for(int i=0;i<arr.length;i++){
+            table.put(arr[i],1);
+        }
+        for(int i=0;i<arr.length; i++){
+            if(table.get(arr[i]+k) != null){
+                c++;
+            }
+        }
+        return c;
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        String[] nk = scanner.nextLine().split(" ");
+
+        int n = Integer.parseInt(nk[0]);
+
+        int k = Integer.parseInt(nk[1]);
+
+        int[] arr = new int[n];
+
+        String[] arrItems = scanner.nextLine().split(" ");
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int i = 0; i < n; i++) {
+            int arrItem = Integer.parseInt(arrItems[i]);
+            arr[i] = arrItem;
+        }
+
+        int result = pairs(k, arr);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedWriter.close();
+
+        scanner.close();
+    }
+}
